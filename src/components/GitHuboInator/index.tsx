222644220f60ab5b94f4ac8f34d78d1e2bef958d
kartonixon <adamkartanowicz@gmail.com>
2021-04-24 11:05:19 +0200

    connect everything in main component

--- src/components/GitHuboInator/index.tsx
+++ src/components/GitHuboInator/index.tsx
@@ -1,9 +1,48 @@
-import { FunctionComponent } from "react";
+import { Typography } from "antd";
+import { useForm } from "antd/lib/form/Form";
+import { FunctionComponent, useState } from "react";
+import { fetchRepositories, Repository } from "../../Repository";
+import { RepositoryTile } from "../RepositoryTile";
+import { UsernameForm, UsernameFormFields } from "../UsernameForm";
+import styles from "./styles.module.scss";
+
+const { Title } = Typography;
 
 export const GitHuboInator: FunctionComponent = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [form] = useForm();
+
+  const onFormFinish = async (values: UsernameFormFields) => {
+    setIsLoading(true);
+    const repos = await fetchRepositories(values.username);
+    setRepositories(repos);
+    setIsLoading(false);
+  };
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Title>Fetch Repositories</Title>
+
+      <div className={styles.formWrapper}>
+        <UsernameForm
+          isLoading={isLoading}
+          form={form}
+          onFinish={onFormFinish}
+        />
+      </div>
+
+      {repositories.length > 0 ? (
+        repositories
+          .sort((a, b) => b.stars - a.stars)
+          .map((repo, idx) => (
+            <div key={idx} className={styles.tile}>
+              <RepositoryTile repo={repo} />
+            </div>
+          ))
+      ) : (
+        <p>Empty :(</p>
+      )}
     </div>
   );
 };
